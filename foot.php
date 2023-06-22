0a9dad9a97690fe4ce871655fd29cc8a0c3798d6
Saipunya <sumetchoorat@gmail.com>
2023-06-22 08:00:36 +0700

    update data

--- foot.php
+++ foot.php
@@ -1,7 +1,7 @@
  <!-- footer -->
             <!-- ============================================================== -->
             <footer class="footer text-center">
-                All Rights Reserved by Matrix-admin. Designed and Developed by <a
+                test All Rights Reserved by Matrix-admin. Designed and Developed by <a
                     href="https://www.wrappixel.com">WrapPixel</a>.
             </footer>
             <!-- ============================================================== -->
